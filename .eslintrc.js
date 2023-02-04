00bcc669794a142ca39b894220479e4d9e2e2171
laizhixin <2469283352@qq.com>
2023-02-05 01:59:03 +0800

    小修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "vue/multi-word-component-names": "off",
+    "vue/return-in-computed-property": "off",
+    "vue/require-prop-type-constructor": "off",
+    "no-case-declarations": "off",
+    "no-useless-escape": "off",
+    "no-irregular-whitespace": "off",
+    "no-unused-vars": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    parser: "babel-eslint",
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,22 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
-import elementUi from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import axios from './api/api'
-import ECharts from 'vue-echarts'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
+import elementUi from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import axios from "./api/api";
+import ECharts from "vue-echarts";
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(elementUi);
-Vue.component('v-chart', ECharts)
+Vue.component("v-chart", ECharts);
 
 new Vue({
-    router,
-    store,
-    axios,
-    render: h => h(App)
-}).$mount('#app')
+  router,
+  store,
+  axios,
+  render: (h) => h(App),
+}).$mount("#app");
